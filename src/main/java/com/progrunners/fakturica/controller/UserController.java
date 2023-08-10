782b2a87072b8d8cbc914263df53aba645d09fee
Luka Matic <maticluka888@gmail.com>
2023-08-10 18:52:07 +0200

    addin user.html page nad user controller

--- src/main/java/com/progrunners/fakturica/controller/UserController.java
+++ src/main/java/com/progrunners/fakturica/controller/UserController.java
@@ -1,5 +1,16 @@
 package com.progrunners.fakturica.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class UserController
 {
+
+    @GetMapping("/")
+    public String showMain() {
+        return "user/main";
+    }
 }
