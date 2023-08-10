3c4b61dc67faf6d7ea87981131e6e7be1aed509d
Luka Matic <maticluka888@gmail.com>
2023-08-10 15:26:49 +0200

    real initital commit

---
+++ src/main/java/com/progrunners/fakturica/FakturicaApplication.java
@@ -0,0 +1,13 @@
+package com.progrunners.fakturica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakturicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakturicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progrunners/fakturica/FakturicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progrunners.fakturica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakturicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
